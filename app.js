6a67f5d9a594ff853af936f3adab9f8f22909a22
Murad Ahmed <murad@linkstaff.co.jp>
2023-08-06 21:57:43 +0600

    code updated

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const fileUpload = require('express-fileupload');
 
 const port = 4000
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bejzy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bejzy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://murad97dev:SIMregiM1@cluster0.e4wbkw7.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
